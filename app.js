8e382997418c5f964290c325547181c0911195ca
admin <Mistermo716@gmail.com>
2018-01-15 13:06:58 -0800

    Try block and Error

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 function whoAmI(name = "Mo", age = 24) {
+    const yob = yearOfBirth(age);
     console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
-    yearOfBirth()
+    console.log(`I was born in ${yob}.`);
 }
 
-function yearOfBirth(){
-    const yearOfBirth = 2018 - age; 
-    console.log(`I was born in ${yearOfBirth}.`);
-}
 
-whoAmI('Zhou', 25);
+function yearOfBirth(age)
+{
+    if (age < 0)
+    {
+        throw new Error ("Age can not be negative");
+    }
+    return 2018 - age;
+}
+try {
+whoAmI('Zhou', -25);
+}
+catch (e) {
+    console.error(e.message);
+}
